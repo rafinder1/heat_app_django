c22ec2bdd1da2f2d4ea28a99b3ba0dac876dbf58
rafinder1 <rafpro33@gmail.com>
2023-08-23 21:49:03 +0200

    create model and add data from admin interface

---
+++ heat/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import TypeLayer, BuildingPartition
+
+
+@admin.register(TypeLayer)
+class TypeLayerAdmin(admin.ModelAdmin):
+    list_display = ('type_layer',)
+
+
+@admin.register(BuildingPartition)
+class BuildingPartitionAdmin(admin.ModelAdmin):
+    list_display = ('name_layer', 'type_layer', 'thickness', 'thermal_conductivity', 'cost')
+    list_filter = ('type_layer',)
+    search_fields = ('name_layer',)
---
+++ heat/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class TypeLayer(models.Model):
+    type_layer = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.type_layer
+
+
+class BuildingPartition(models.Model):
+    type_layer = models.ForeignKey(TypeLayer, on_delete=models.CASCADE)
+    name_layer = models.CharField(max_length=100)
+    thickness = models.FloatField()
+    thermal_conductivity = models.FloatField()
+    cost = models.FloatField()
+
+    def __str__(self):
+        return self.name_layer
