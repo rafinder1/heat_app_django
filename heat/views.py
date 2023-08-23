408565739701ffede0defce5f4044f5d57ee66a9
rafinder1 <rafpro33@gmail.com>
2023-08-23 22:45:00 +0200

    create app view - djangorestframework

--- heat/views.py
+++ heat/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from heat.models import Material, TypeLayer
+
 
 # Create your views here.
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        'api/materials',
+        'api/type_layers'
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def get_materials(request):
+    materials = Material.objects.all()
+    return Response({'materials': serializers.serialize('json', materials)})
