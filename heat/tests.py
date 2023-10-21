6e01db923a72509838079b921288041aeecd5652
rafinder1 <rafpro33@gmail.com>
2023-10-21 18:04:41 +0200

    add test model Type layer

--- heat/tests.py
+++ heat/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
+from .models import TypeLayer
+
+
 # Create your tests here.
+
+class TypeLayerTestCase(TestCase):
+    def test_type_layer_creation(self):
+        # Create a TypeLayer instance
+        type_layer = TypeLayer.objects.create(type_layer="mur")
+
+        # Check if the instance was created successfully
+        self.assertEqual(type_layer.type_layer, "mur")
+
+    def test_unique_constraint(self):
+        # Create a TypeLayer instance with a unique value
+        unique_type_layer = TypeLayer(type_layer="mur")
+        unique_type_layer.save()
+
+        # Try to create another instance with the same value, which should raise an IntegrityError
+        with self.assertRaises(Exception):
+            duplicate_type_layer = TypeLayer(type_layer="mur")
+            duplicate_type_layer.save()
