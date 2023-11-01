a54c156fb08c085475f59f2d67e2090b241b75a0
rafinder1 <rafpro33@gmail.com>
2023-11-01 21:20:28 +0100

    update package temperature-calculator and fix amount polystyrene and price

--- heat/calculator/amount_polystyrene_and_price.py
+++ heat/calculator/amount_polystyrene_and_price.py
@@ -1,12 +1,12 @@
-from heat.calculator.APAP_data import AmountPolystyreneData
 from calculator.amount_polystyrene.amount_polystyrene import AmountPolystyreneAndPrice
+from heat.calculator.APAP_data import AmountPolystyreneData
 
 
 class AmountPolystyreneAndPriceCalculator:
 
     @classmethod
     def calculate(cls, amount_polystyrene_data: AmountPolystyreneData):
-        data = AmountPolystyreneAndPrice.calculate(wall_surface=amount_polystyrene_data.wall_surface,
-                                                   price_square_meter=amount_polystyrene_data.price_square_meter,
-                                                   amount_package=amount_polystyrene_data.amount_package)
-        return data
+        return AmountPolystyreneAndPrice.calculate(
+            wall_surface=float(amount_polystyrene_data.wall_surface),
+            price_square_meter=float(amount_polystyrene_data.price_square_meter),
+            amount_polystyrene_in_one_package=float(amount_polystyrene_data.amount_package))
